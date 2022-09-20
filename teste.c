736db5986320f0db8661c98e2489706f850b23dd
Thiago Martins <31443982+thiago0003@users.noreply.github.com>
2022-09-20 17:31:19 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,54 +1,32 @@
 
 #include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/interrupt.h>
-#include <linux/delay.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-MODULE_LICENSE("GPL");
-#define KBD_DATA_REG        0x60    /* I/O port for keyboard data */
-#define KBD_CONTROL_REG        0x64 
-#define KBD_SCANCODE_MASK   0x7f
-#define KBD_STATUS_MASK     0x80
-#define DELAY do { mdelay(1); if (++delay > 10) break; } while(0)
-static void set_kbd_led_on(void)
+#define __NR_identity 440
+
+long identity_syscall(void)
 {
-	long delay = 0;
-	//wait till the input buffer is empty
-	while (inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0xED, KBD_DATA_REG);
-	DELAY;
-	while (inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0x07, KBD_DATA_REG);
-	DELAY;
-}
-static void set_kbd_led_off(void)
-{
-	long delay = 0;
-	//wait till the input buffer is empty
-	while (inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0xED, KBD_DATA_REG);
-	DELAY;
-	while (inb(KBD_CONTROL_REG) & 0x02)
-		DELAY;
-	outb(0x00, KBD_DATA_REG);
-	DELAY;
+    return syscall(__NR_identity);
 }
 
-static int test_kbd_led_init(void)
+int main(int argc, char *argv[])
 {
-        pr_info("%s: In init\n", __func__);
-	set_kbd_led_on();
-	return 0;
-}
+    long activity;
+    activity = identity_syscall();
 
-static void test_kbd_led_exit(void)
-{
-        pr_info("%s: In exit\n", __func__);
-	set_kbd_led_off();
-}
+    if(activity < 0)
+    {
+        perror("Sorry, Jasper. Your system call appears to have failed.");
+    }
 
-module_init(test_kbd_led_init);
-module_exit(test_kbd_led_exit);
+    else
+    {
+        printf("Congratulations, Jasper! Your system call is functional. Run the command dmesg in the terminal and find out!\n");
+    }
+
+    return 0;
+}
